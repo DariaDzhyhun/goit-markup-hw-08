5d5e7b764920cbb98b4b1076019904a2f64a8efc
DariaDzhyhun <dzhigun21@gmail.com>
2022-10-01 18:44:16 +0300

    update

--- mobile-menu.js
+++ mobile-menu.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector("[data-menu-open]"),
     closeModalBtn: document.querySelector("[data-menu-close]"),
     modal: document.querySelector("[data-menu]"),
+    bodyNoScroll = document.querySelector("[data-no-scroll]");
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
@@ -10,5 +11,6 @@
 
   function toggleModal() {
     refs.modal.classList.toggle("is-open");
+    refs.bodyNoScroll.classList.toggle('no-scroll');
   }
 })();
